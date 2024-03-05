027badfafd20ce45816225d7a0165ca508c1b69f
Rahul2024 <73351830+Rahul2024@users.noreply.github.com>
2024-03-05 07:33:31 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:249296835135:web:e5b6177334bb77d2299e8a";
+  static String apiKey = "AIzaSyBvZgKZ_JVzOqCxjK0Xg36eDSFaT2OZ2UE";
+  static String messagingSenderId = "249296835135";
+  static String projectId = "chat-app-8e01e";
+  final primaryColor = const Color(0xFF1DE9B6);
+}
